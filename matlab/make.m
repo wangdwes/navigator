2099f9fcceab7d550904c18371e6ceafd4617983
Dawei <wangdwes@gmail.com>
2015-05-02 09:25:14 -0400

    removed library path for sbpl.

--- matlab/make.m
+++ matlab/make.m
@@ -1,2 +1,2 @@
-mex ../src/navigator_mex.cc ../src/navigator.cc -L../libnavigator -I../src/ -lsbpl -L/usr/local/lib/libsbpl
+mex ../src/navigator_mex.cc ../src/navigator.cc -L../libnavigator -I../src/ -lsbpl
 
